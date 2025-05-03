df05f7488785e7aec58c1be31c1b5eccd96e4f8b
madhumathiudhayakumar <madhumathiudhayakumar7@gmail.com>
2025-05-03 16:53:46 +0530

    deploy to vercel

--- src/routes/privateRoute.js
+++ src/routes/privateRoute.js
@@ -3,7 +3,6 @@ import { Navigate } from 'react-router-dom';
 const PrivateRoute = ({ children }) => {
   const storedData = JSON.parse(localStorage.getItem("userCredentials"))
   const checksIsLogin = Object.values(storedData).every(value => value !== "");
-  
   return checksIsLogin ? children : <Navigate to="/" />;
 };
 
